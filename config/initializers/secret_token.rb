e0f53028470440a4861840e98d806f4d00dba457
panax <butipanax@hotmail.com>
2011-05-12 21:10:48 +0200

    add rails initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeInternationalECommerce::Application.config.secret_token = '6e2c68679d4cd3042e26f33d926435d6aaf89461385423ea83203586445933413d8ff27b5fd4e43e76dce3b28532580e9b6bb72a59bfa694ef3a216c0d0e0589'
