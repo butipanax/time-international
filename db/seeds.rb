0745f26a305c1816933489d6b4d5280308801fd2
panax <butipanax@hotmail.com>
2011-05-25 20:19:17 +0200

    new beginning

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+
+# encoding: utf-8
+
+Role.delete_all
+Role.create(:name => 'admin')
+Role.create(:name => 'staff')
+Role.create(:name => 'customer')
+
+OrderStatus.delete_all
+OrderStatus.create(:name => 'newly created')
+OrderStatus.create(:name => 'paid')
+OrderStatus.create(:name => 'delivered')
+OrderStatus.create(:name => 'canceld')
+OrderStatus.create(:name => 'successfully closed')
+
+DiscountDetail.delete_all
+DiscountDetail.create(:discount_rate => 1)
+DiscountDetail.create(:discount_rate => 0.85)
+DiscountDetail.create(:discount_rate => 0.7)
+
+SerialNumber.delete_all
+SerialNumber.create(:serial_number_name => 'used_by_invoice_number', :current_max_number=>1)
